5e4fbae9303b56572442afc144115dfa66b6a906
Aleksandr <aleksanches123@mail.ru>
2022-12-09 13:42:49 +0300

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
     }
 
     public static void task1() {
         System.out.println("Задача 1");
         double total = 0;
         int months = 0;
-        while(total < 2_459_000){
+        while (total < 2_459_000) {
             total = total + 15000 + (total * 0.01);
             months++;
             System.out.println(total);
@@ -21,6 +22,7 @@ public class Main {
         System.out.println(months);
 
     }
+
     public static void task2() {
         System.out.println("Задача 2");
         int a = 1;
@@ -29,25 +31,27 @@ public class Main {
             a++;
         }
         System.out.println();
-        for(int i = 10; i > 0; i--) {
+        for (int i = 10; i > 0; i--) {
             System.out.print(i + " ");
         }
     }
+
     public static void task3() {
         System.out.println("Задача 3");
         int population = 1000;
         int wasBorn = 17;
         int died = 8;
-        for(int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             population = population + wasBorn - died;
             System.out.println("Год " + i + ", численность населения составляет " + population);
         }
     }
+
     public static void task4() {
         System.out.println("Задача 4");
         int total = 15000;
         int months = 0;
-        while(total < 12_000_000) {
+        while (total < 12_000_000) {
             total = total + (total / 100 * 7);
             months++;
             System.out.println(total);
@@ -55,6 +59,7 @@ public class Main {
         }
         System.out.println(months);
     }
+
     public static void task5() {
         System.out.println("Задача 5");
         int total = 15000;
@@ -62,24 +67,26 @@ public class Main {
         while (total < 12_000_000) {
             total = total + (total / 100 * 7);
             months++;
-            if(months % 6 == 0) {
+            if (months % 6 == 0) {
                 System.out.println(total);
             }
         }
         System.out.println(months);
     }
+
     public static void task6() {
         System.out.println("Задача 6");
         int total = 15000;
         int months = 12 * 9;
-        for(int i = 1; i < months; i++) {
+        for (int i = 1; i < months; i++) {
             total = total + (total / 100 * 7);
-            if(i % 6 == 0) {
+            if (i % 6 == 0) {
                 System.out.println(total);
             }
         }
         System.out.println(months);
     }
+
     public static void task7() {
         System.out.println("Задача 7");
         int day = 5;
@@ -88,4 +95,19 @@ public class Main {
             day = day + 7;
         }
     }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int age = 0;
+        int age1 = 1822;
+        int age2 = 2122;
+        while (age < age2) {
+            age = age + 79;
+            if (age >= age1 && age <= age2) {
+                System.out.println(age);
+            }
+        }
+
+    }
+
 }
