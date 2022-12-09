5b9284970df2849627a2a2b46fae2028743d2a8b
Aleksandr <aleksanches123@mail.ru>
2022-12-09 12:59:00 +0300

    Задача 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1() {
@@ -48,6 +49,20 @@ public class Main {
             total = total + 15000 + (total * 0.07);
             months++;
             System.out.println(total);
+
+        }
+        System.out.println(months);
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        double total = 0;
+        int months = 0;
+        while (total < 12_000_000) {
+            total = total + 15000 + (total * 0.07);
+            months++;
+            if(months % 6 == 0) {
+                System.out.println(total);
+            }
         }
         System.out.println(months);
     }
